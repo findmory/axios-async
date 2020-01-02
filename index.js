392ae8094c0b04c18374b9a9013bb51da29f3617
findmory <findmory@gmail.com>
2020-01-02 18:10:20 -0500

    added all the params

--- index.js
+++ index.js
@@ -2,65 +2,42 @@ const delay = require("delay");
 const axios = require("axios");
 
 module.exports = async config => {
-  // destructure config
-  let {
-    url,
-    method,
-    data,
-    timeout,
-    retryDelay,
-    retryingCallback,
-    retryType,
-    retries,
-    successCodes
-  } = config;
-
   // url is required.  do something if we don't have it
-  config = {
-    url: url,
-    method: method || "get",
-    data: data || null,
-    timeout: timeout || 3000,
-    retryDelay: retryDelay || 200,
-    retryingCallback: retryingCallback || null,
-    retryType: retryType || "static",
-    retries: retries || 0,
-    successCodes: successCodes || []
+  
+  let requiredParms = {
+    url: config.url,
+    method: config.method || "get",
+    timeout: config.timeout || 3000,
+    retryDelay: config.retryDelay || 200,
+    retryType: config.retryType || "static",
   };
+
+  let _config = {...config, ...requiredParms};
 
   let retryLooper = true;
   let retryCount = 0;
-  let retryDelayCalc = config.retryDelay;
+  let retryDelayCalc = _config.retryDelay;
   let resp;
   do {
-    resp = await _axiosAsync(config);
+    resp = await _axiosAsync(_config);
     if (!resp) {
-      // are we out of retries?
-      
-      if (retryCount >= config.retries && config.retries !== -1) {
-        retryLooper = false;
-        continue;
-      }
       retryCount++;
-      config.retryingCallback ? config.retryingCallback() : null;
+      _config.retryingCallback ? _config.retryingCallback() : null;
 
-      switch (retryType) {
+      switch (_config.retryType) {
         case "exponential":
           retryDelayCalc = ((Math.pow(2, retryCount) - 1) / 2) * 1000;
           break;
         case "linear":
-          retryDelayCalc = retryCount * config.retryDelay;
+          retryDelayCalc = retryCount * _config.retryDelay;
           break;
         default:
-          retryDelayCalc = config.retryDelay;
+          retryDelayCalc = _config.retryDelay;
           break;
       }
-
-    
       console.log(`delaying for ${retryDelayCalc}`);
       await delay(retryDelayCalc);
       retryLooper = true;
-      
     } else {
       retryLooper = false;
     }
@@ -69,28 +46,11 @@ module.exports = async config => {
 };
 
 _axiosAsync = async config => {
-  let _config = {};
-  _config.url = config.url;
-  _config.method = config.method;
-  _config.timeout = config.timeout;
-  if (config.data) {
-    _config.data = config.data;
-  }
   try {
-    let resp = await axios(_config);
+    let resp = await axios(config);
     return resp;
   } catch (err) {
     console.log("handling the error");
-    // console.log(err.response.status);
-    // console.log(err.response.data);
-
-    // even though certain errors are caught we can consider them success
-    if (
-      err.response &&
-      config.successCodes.includes(err.response.status)
-    ) {
-      return err.response;
-    }
     return false;
   }
 };
